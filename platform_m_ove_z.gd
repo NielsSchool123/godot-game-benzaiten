530e0c001eeb771c0868addfb86a7323e2aea0c2
NielsvanderPijl <199636@novacollege.nl>
2025-04-08 14:22:11 +0200

    level 3 further things

---
+++ platform_m_ove_z.gd
@@ -0,0 +1,19 @@
+extends StaticBody3D
+
+@export var move_distance: float = 7.5  # Distance forward/backward
+@export var move_speed: float = 10.0  # Speed of movement
+
+var direction: int = 1  # 1 = forward, -1 = backward
+var start_position: Vector3
+
+func _ready():
+	start_position = global_transform.origin  # Store starting position
+
+func _process(delta):
+	global_transform.origin.z += direction * move_speed * delta  # Move along Z-axis
+
+	# Reverse direction when reaching limits
+	if global_transform.origin.z >= start_position.z + move_distance:
+		direction = -1  # Move backward
+	elif global_transform.origin.z <= start_position.z - move_distance:
+		direction = 1  # Move forward
