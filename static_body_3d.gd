319f1f85b440432b380f170d785323df550f9caf
NielsvanderPijl <199636@novacollege.nl>
2025-04-11 11:06:43 +0200

    sound

--- static_body_3d.gd
+++ static_body_3d.gd
@@ -1,4 +1,5 @@
 extends Area3D
+@onready var Death = $Death
 
 func _ready():
 	body_entered.connect(_on_body_entered)  # dectecteer of iets de hitbox raakt 
@@ -6,5 +7,6 @@ func _ready():
 
 func _on_body_entered(body: Node3D):
 	if body.is_in_group("player"):
+		Death.play()
 		await get_tree().create_timer(0.16).timeout
 		get_tree().reload_current_scene()  # reset de game (mischien veranderen in de toekomst)
