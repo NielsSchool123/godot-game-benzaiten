075feb5939f9cd90ec34064fc693ddba0e721b67
NielsvanderPijl <199636@novacollege.nl>
2025-04-12 21:13:45 +0200

    tooltip fix

--- scripts/intro_screen.gd
+++ scripts/intro_screen.gd
@@ -1,25 +1,7 @@
 extends Control
+
 @onready var button_select: AudioStreamPlayer = $Buttonselect
 @onready var secret_label = $Secretslabel
- # logica voor de quit button, als de button text quit is dan runt het de logica om het af te sluiten en aders gebruikt de de funcite daaronder
-func _ready():
-	secret_label.text = "Secrets found: " + str(GameData.secrets_found) + "/" + str(GameData.total_secrets)
-	for button in $VBoxContainer.get_children():
-		if button.name == "Quit":
-			button.pressed.connect(func(): get_tree().quit())
-		else:
-			button.pressed.connect(func(): _on_level_selected(button.name))
-	var random_tip = tooltips[randi() % tooltips.size()]
-	tooltip_label.text = random_tip
-
-
-func _on_level_selected(level_name: String): # logica voor levels laden, het kijkt hij de button heet  en laad dat level
-	button_select.play()
-	await get_tree().create_timer(0.14).timeout
-	var scene_path = "res://" + level_name + ".tscn" 
-	get_tree().change_scene_to_file(scene_path)
-
-
 @onready var tooltip_label = $TooltipLabel
 
 var tooltips = [
@@ -28,5 +10,27 @@ var tooltips = [
 	"Jumping just before falling off a ledge gives you more distance.",
 	"Some platforms aren't as stable as they look...",
 	"You can rotate mid-air—use it to align for tight landings.",
+	"Some things might be locked away forever if your not careful enough...",
 	"Some platforms might not be real. Be aware of details!"
 ]
+
+
+# logica voor de quit button, als de button text quit is dan runt het de logica om het af te sluiten en aders gebruikt de de funcite daaronder
+func _ready():
+	secret_label.text = "Secrets found: " + str(GameData.secrets_found) + "/" + str(GameData.total_secrets)
+	for button in $VBoxContainer.get_children():
+		if button.name == "Quit":
+			button.pressed.connect(func(): get_tree().quit())
+		else:
+			button.pressed.connect(func(): _on_level_selected(button.name))
+	if GameData.tooltip_queue.is_empty():
+		GameData.refill_tooltip_queue(tooltips)
+
+	tooltip_label.text = GameData.tooltip_queue.pop_front()
+
+
+func _on_level_selected(level_name: String): # logica voor levels laden, het kijkt hij de button heet  en laad dat level
+	button_select.play()
+	await get_tree().create_timer(0.14).timeout
+	var scene_path = "res://" + level_name + ".tscn" 
+	get_tree().change_scene_to_file(scene_path)
--- scripts/secretcounter.gd
+++ scripts/secretcounter.gd
@@ -2,3 +2,9 @@ extends Node
 
 var secrets_found: int = 0
 var total_secrets: int = 1
+
+var tooltip_queue: Array = []
+
+func refill_tooltip_queue(tooltips: Array):
+	tooltip_queue = tooltips.duplicate()
+	tooltip_queue.shuffle()
