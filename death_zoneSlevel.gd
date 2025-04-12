c1fb7c7f4837e1580f57de43c446c2ac082b16d8
NielsvanderPijl <199636@novacollege.nl>
2025-04-12 14:33:23 +0200

    mouse locked bug fix

--- death_zoneSlevel.gd
+++ death_zoneSlevel.gd
@@ -6,8 +6,10 @@ extends Area3D
 func _ready():
 	body_entered.connect(_on_body_entered)  # dectecteer of iets de hitbox raakt 
 
+
 func _on_body_entered(body):
 	death.play()
+	await get_tree().create_timer(0.16).timeout
 	if body.is_in_group("player"):
-		await get_tree().create_timer(0.16).timeout
-	get_tree().change_scene_to_file(main_menu_scene)
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)  # free the mouse
+		get_tree().change_scene_to_file(main_menu_scene)
