e86ddc7d3fb4b660ea7c9405dabc1167d683766f
NielsvanderPijl <199636@novacollege.nl>
2025-04-13 13:53:24 +0200

    menu fix

--- pause_menu.gd
+++ pause_menu.gd
@@ -13,20 +13,21 @@ func _ready():
 
 
 func _on_resume_pressed():
+	Engine.time_scale = 1
 	button_select.play()
 	await get_tree().create_timer(0.14).timeout
-	Engine.time_scale = 1
 	self.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _on_main_menu_pressed():
+	Engine.time_scale = 1
 	button_select.play()
 	await get_tree().create_timer(0.14).timeout
-	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://MainMenu.tscn")  # Adjust to your main menu path
 
 func _on_settings_pressed():
+	Engine.time_scale = 1
 	button_select.play()
 	await get_tree().create_timer(0.14).timeout
-	Engine.time_scale = 1
+	
 	get_tree().change_scene_to_file("res://Settings.tscn")
