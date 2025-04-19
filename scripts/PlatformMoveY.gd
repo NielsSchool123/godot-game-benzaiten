82da9651c5fba2f68ef96b9f77c949ddd0b4a1e4
NielsvanderPijl <199636@novacollege.nl>
2025-04-19 20:55:04 +0200

    platform y 4 fix

--- scripts/PlatformMoveY.gd
+++ scripts/PlatformMoveY.gd
@@ -1,19 +1,18 @@
 extends AnimatableBody3D
 
-@export var move_distance: float = 15  # hoe ver
-@export var move_speed: float = 15  # hoe snel
+@export var move_distance: float = 15
+@export var move_speed: float = 15
 
 var direction: int = 1
 var start_position: Vector3
 
 func _ready():
-	start_position = global_transform.origin  # start positie bewaren
+	start_position = global_transform.origin
 
 func _process(delta):
-	global_transform.origin.y += direction * move_speed * delta
+	global_transform.origin += Vector3(0, direction * move_speed * delta, 0)
 
-	# veranderd waar die naar toe gaat wanneer het limiet is gehaald
 	if global_transform.origin.y >= start_position.y + move_distance:
-		direction = -1  
-	elif global_transform.origin.y <= start_position.y - start_position.y:
-		direction = 1  
+		direction = -1
+	elif global_transform.origin.y <= start_position.y:
+		direction = 1
