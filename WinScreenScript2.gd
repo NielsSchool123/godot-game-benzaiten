adc0f7b81928c4b2ff42dd31f80d30cdf2906361
NielsvanderPijl <199636@novacollege.nl>
2025-04-20 14:49:09 +0200

    level 5 fix + next button fix

--- WinScreenScript2.gd
+++ WinScreenScript2.gd
@@ -6,7 +6,6 @@ extends Control
 @onready var button_select: AudioStreamPlayer = $Buttonselect
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	win_sound.play()
 	$Button.pressed.connect(_on_button_pressed)
 	$Button2.pressed.connect(_on_button2_pressed)
