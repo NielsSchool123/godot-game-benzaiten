0896d9e8889ecf30b79921169549a44024bf5256
NielsvanderPijl <199636@novacollege.nl>
2025-04-23 13:20:53 +0200

    laser fix

--- scripts/Laser.gd
+++ scripts/Laser.gd
@@ -18,9 +18,9 @@ func _process(delta):
 		direction *= -1
 
 func _on_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not body.is_dead:
 		if body.has_method("die"):
 			body.die()
 			death.play()
-			await get_tree().create_timer(3.0).timeout  # wacht 3 seconden zodat animatie afspeelt
+			await get_tree().create_timer(3.0).timeout
 			get_tree().reload_current_scene()
