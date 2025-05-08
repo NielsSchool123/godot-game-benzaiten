5d74b6832afe93ba816b6845a8e1141a01cea8c2
NielsvanderPijl <199636@novacollege.nl>
2025-05-08 22:20:08 +0200

    level 10 rups fix

--- scripts/rups_gast.gd
+++ scripts/rups_gast.gd
@@ -1,14 +1,19 @@
 extends Node3D
 
-@export var move_distance := 2.0
-@export var move_speed := 5.0
+@export var move_distance := 5
+@export var move_speed := 15
 
 var target_position: Vector3
-
 
 func _ready():
 	target_position = global_transform.origin
 
+func _process(delta):
+	# Move towards the target position smoothly
+	var current_position = global_transform.origin
+	var new_position = current_position.lerp(target_position, move_speed * delta)
+	global_transform.origin = new_position
+
 func _on_area_3d_body_entered(body):
-	if body.name == "Player": # or check with body is Player if you use a script
+	if body.name == "player":
 		target_position = global_transform.origin - Vector3(0, move_distance, 0)
