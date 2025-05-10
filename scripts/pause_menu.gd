6f206e7861060e51a6c7b35f65b2e74de52ee7df
NielsvanderPijl <199636@novacollege.nl>
2025-05-10 18:24:17 +0200

    next level button fix and some level fixes

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -2,14 +2,14 @@ extends CanvasLayer
 
 @onready var resume_button: Button = $Control/ResumeButton
 @onready var main_menu_button: Button = $Control/MainMenuButton
-@onready var settings_button: Button = $Control/QuitButton
+@onready var quit_button: Button = $Control/QuitButton
 @onready var button_select: AudioStreamPlayer = $Buttonselect
 
 
 func _ready():
 	resume_button.pressed.connect(_on_resume_pressed)
 	main_menu_button.pressed.connect(_on_main_menu_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 
 
 func _on_resume_pressed():
@@ -25,7 +25,7 @@ func _on_main_menu_pressed():
 	await get_tree().create_timer(0.14).timeout
 	get_tree().change_scene_to_file("res://MainMenu.tscn")  # Adjust to your main menu path
 
-func _on_settings_pressed():
+func _on_quit_pressed():
 	Engine.time_scale = 1
 	button_select.play()
 	await get_tree().create_timer(0.14).timeout
--- scripts/teleporter.gd
+++ scripts/teleporter.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 
 var can_teleport := true  # cooldown flag
+@onready var teleport_sound = $Area3D/Teleport
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	# Check that the body is the player (by group, more reliable than name)
@@ -35,5 +36,5 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		print("No valid teleport destination found")
 
 	# Optional cooldown delay to prevent bouncing
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(1).timeout
 	can_teleport = true
